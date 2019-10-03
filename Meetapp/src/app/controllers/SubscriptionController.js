7b93a25be1cd26957a5a2d51601c0baf00425b4d
Vitor <vitorvcs@hotmail.com>
2019-10-03 11:32:18 -0300

    Desafio 3 code

--- Meetapp/src/app/controllers/SubscriptionController.js
+++ Meetapp/src/app/controllers/SubscriptionController.js
@@ -1,7 +1,32 @@
+import { Op } from 'sequelize';
 import User from '../models/User';
 import Meetup from '../models/Meetup';
+import Subscription from '../models/Subscription';
+import Queue from '../../lib/Queue';
 
 class SubscriptionController {
+  async index(req, res) {
+    const subscriptions = await Subscription.findAll({
+      where: {
+        user_id: req.userId,
+      },
+      include: [
+        {
+          model: Meetup,
+          where: {
+            date: {
+              [Op.gt]: new Date(),
+            },
+          },
+          required: true,
+        },
+      ],
+      order: [[Meetup, 'date']],
+    });
+
+    return res.json(subscriptions);
+  }
+
   async store(req, res) {
     const user = await User.findByPk(req.userId);
     const meetup = await Meetup.findByPk(req.params.meetupId, {
@@ -11,14 +36,45 @@ class SubscriptionController {
     if (meetup.userId === req.userId) {
       return res
         .status(400)
-        .json({ error: "Can't subscribe to you own meetups" });
+        .json({ error: "Can't subscribe to your own meetups" });
     }
 
     if (meetup.past) {
       return res.status(400).json({ error: "Can't subscribe to past meetups" });
     }
 
-    return res.json();
+    const checkDate = await Subscription.findOne({
+      where: {
+        user_id: user.id,
+      },
+      include: [
+        {
+          model: Meetup,
+          required: true,
+          where: {
+            date: meetup.date,
+          },
+        },
+      ],
+    });
+
+    if (checkDate) {
+      return res
+        .status(400)
+        .json({ error: "Can't subscribe to two meetups at the same time" });
+    }
+
+    const subscription = await Subscription.create({
+      user_id: user.id,
+      meetup_id: meetup.id,
+    });
+
+    await Queue.add(Subscription.key, {
+      meetup,
+      user,
+    });
+
+    return res.json(subscription);
   }
 }
 
