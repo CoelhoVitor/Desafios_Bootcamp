0eb6a4f96f9e5b3c6ce498f944bf4657ad25d440
Vitor <vitorvcs@hotmail.com>
2019-10-04 16:35:11 -0300

    auth

--- Meetapp/src/config/auth.js
+++ Meetapp/src/config/auth.js
@@ -1,5 +1,5 @@
 export default {
-  secret: process.env.APP_SECRET,
-  // secret: '1bb3ffd7b45642874c633f828eeab68e',
+  // secret: process.env.APP_SECRET,
+  secret: '1bb3ffd7b45642874c633f828eeab68e',
   expiresIn: '7d',
 };
--- Meetapp/src/config/database.js
+++ Meetapp/src/config/database.js
@@ -1,13 +1,15 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'mysql',
-  // host: process.env.DB_HOST,
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  // database: process.env.DB_NAME,
-  host: 'localhost',
-  username: 'root',
-  password: 'batis',
-  database: 'meetapp',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  // host: 'localhost',
+  // username: 'root',
+  // password: 'batis',
+  // database: 'meetapp',
   operatorAliases: false,
   define: {
     timestamps: true,
